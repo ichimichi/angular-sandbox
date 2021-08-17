50a9c8e5e3d295b713725f6bc3e451c6c85285ca
ichimichi <ichimichilaribok@gmail.com>
2021-08-17 16:08:36 +0530

    rendering a list using ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>{{ title }}</h1>`,
+  template: `
+    <h1>{{ title }}</h1>
+    <app-habit-list></app-habit-list>
+  `,
   styles: ['h1 { color: blue}'],
 })
 export class AppComponent {
