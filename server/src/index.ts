ab80d45199da8cb6e89d46602808a92849b60203
ichimichi <ichimichilaribok@gmail.com>
2021-08-17 20:59:07 +0530

    setup basic server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response } from 'express';
+import { data } from './data';
+import morgan from 'morgan';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const address = `http://localhost:${port}`;
 
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(morgan('dev'));
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello From Express and TS Node server!');
+  res.send('Hello From Express dand TS Node server!');
+});
+
+app.get('/api/habits', (req, res) => {
+  res.send(data.habits);
 });
 
 app
