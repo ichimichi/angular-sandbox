bf94684b6c749b887f193d4615c2a2c994e701ba
ichimichi <ichimichilaribok@gmail.com>
2021-08-29 03:08:05 +0530

    access parent route parameters in child components

--- src/app/system-info/system-info.component.ts
+++ src/app/system-info/system-info.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { SystemService } from '../data/system.service';
 
 @Component({
   selector: 'app-system-info',
   templateUrl: './system-info.component.html',
-  styleUrls: ['./system-info.component.scss']
+  styleUrls: ['./system-info.component.scss'],
 })
 export class SystemInfoComponent implements OnInit {
+  info: Observable<string>;
 
-  constructor() { }
+  constructor(
+    private systemService: SystemService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.info = this.route.parent.paramMap.pipe(
+      map((params: ParamMap) => +params.get('id')),
+      switchMap((id: number) => this.systemService.getSystemInfo(id))
+    );
   }
-
 }
--- src/app/system-items/system-items.component.ts
+++ src/app/system-items/system-items.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Item } from '../data/system';
+import { SystemService } from '../data/system.service';
 
 @Component({
   selector: 'app-system-items',
   templateUrl: './system-items.component.html',
-  styleUrls: ['./system-items.component.scss']
+  styleUrls: ['./system-items.component.scss'],
 })
 export class SystemItemsComponent implements OnInit {
+  items: Observable<Item[]>;
 
-  constructor() { }
+  constructor(
+    private systemService: SystemService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.items = this.route.parent.paramMap.pipe(
+      map((params: ParamMap) => +params.get('id')),
+      switchMap((id: number) => this.systemService.getSystemItems(id))
+    );
   }
-
 }
